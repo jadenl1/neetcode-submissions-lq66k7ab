39ef2077fc78a1ee8d21187d486ea5d2ea658094
Jaden Leonard <91337033+jadenl1@users.noreply.github.com>
2026-08-26 21:27:55 -0400

    Add: level-order-traversal-of-binary-tree - submission-5

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-5.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = deque()
+        q.append(root)
+
+        result = []
+        nodeNum = 0
+
+        while q:
+            currLevel = []
+            while q:
+                currLevel.append(q.popleft())
+
+            for curr in currLevel:
+                if curr:
+                    q.append(curr.left)
+                    q.append(curr.right)
+            
+            currLevelVals = []
+            for node in currLevel:
+                if node:
+                    currLevelVals.append(node.val)
+            if currLevelVals:
+                result.append(currLevelVals)
+
+        return result
