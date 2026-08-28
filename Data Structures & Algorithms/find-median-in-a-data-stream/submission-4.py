1dd65fe4a2e2373bafe814de431de5e574fda48e
Jaden Leonard <91337033+jadenl1@users.noreply.github.com>
2026-08-28 15:37:57 -0400

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.py
@@ -0,0 +1,45 @@
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.left = [] # maxheap
+        self.right = [] # minheap
+        self.median = None
+
+    def addNum(self, num: int) -> None:
+        if self.median is None:
+            heapq.heappush(self.left, -num)
+            self.median = num
+            return
+        
+        if num < self.median:
+            heapq.heappush(self.left, -num)
+        elif num > self.median:
+            heapq.heappush(self.right, num)
+        else:
+            # push to the smaller heap
+            if len(self.left) <= len(self.right):
+                heapq.heappush(self.left, -num)
+            else:
+                heapq.heappush(self.right, num)
+        
+        # now balance trees
+        while len(self.left) > len(self.right):
+            heapq.heappush(self.right, -heapq.heappop(self.left))
+        
+        while len(self.right) > len(self.left):
+            heapq.heappush(self.left, -heapq.heappop(self.right))
+        
+        # print('left', self.left)
+        # print('right', self.right)
+        self.median = self.findMedian()
+            
+
+    def findMedian(self) -> float:
+        if len(self.left) > len(self.right):
+            return -self.left[0]
+        elif len(self.right) > len(self.left):
+            return self.right[0]
+        else:
+            return (-self.left[0] + self.right[0]) / 2
