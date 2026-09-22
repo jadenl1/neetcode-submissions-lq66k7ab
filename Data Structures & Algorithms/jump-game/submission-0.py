d04e3c4fd0e9c9a051a03c161865519d8dae8f31
Jaden Leonard <91337033+jadenl1@users.noreply.github.com>
2026-09-21 22:18:47 -0400

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        memo = dict()
+        
+        for i in range(n-1, -1, -1):
+            for j in range(i, i + nums[i] + 1):
+                if j >= n:
+                    break
+                if j == n-1:
+                    memo[i] = True
+                    break
+                if j in memo and memo[j] == True:
+                    memo[i] = True
+                    break
+                memo[i] = False
+
+        return memo[0]
